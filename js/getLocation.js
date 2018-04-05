75d61aadf5c763ee293eedbfc29235f80ce762bf
ajessefowler <ajessefowler@gmail.com>
2018-04-05 15:27:10 -0400

    finished implementing searchbar, added placeholder current conditions

--- js/getLocation.js
+++ js/getLocation.js
@@ -1,13 +1,13 @@
 
 function getLocation() {
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition, locationError);
+		navigator.geolocation.getCurrentPosition(getWeather, locationError);
 	} else {
 		window.alert('Location not supported. Please enter your location.');
 	}
 }
 
-function showPosition(position) {
+function getWeather(position) {
 	let lat = position.coords.latitude;
 	let long = position.coords.longitude;
 	let key = 'AIzaSyC2Mcoh2tL1KeJUbmn420w0lPvPclJJvMQ';
@@ -16,11 +16,15 @@ function showPosition(position) {
 	
 	// Set the HTML to reflect the retrieved location
 	$.getJSON(url).done(function(location) {
+		
 		let city = location.results[0].address_components[3].long_name;
 		document.getElementById('autocomplete').value = city + ', ' + location.results[0].address_components[5].long_name;
+		document.getElementById('currentheader').innerHTML = 'Currently in ' + city;
+		$('#faveicon').fadeIn();
+
 		$.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=' + weatherKey).done(function(data) {
 			let temp = Math.floor(9 / 5 * (data.main.temp - 273) + 32);
-			$('#description').html('It\'s ' + temp + '°F in ' + city + '.');
+			$('#description').html(temp + '°F');
 		});
 	});
 }
