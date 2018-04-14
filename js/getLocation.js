de041701f72b60d17202379a011bc3f851b4218c
ajessefowler <ajessefowler@gmail.com>
2018-04-14 13:30:20 -0400

    Wind direction is now included in current information. Hourly layout improved by centering content, rather than space-between. Implement radar image on map. Beginning stages of more responsive design.

--- js/getLocation.js
+++ js/getLocation.js
@@ -2,7 +2,7 @@
 	Obtains user's location either automatically or manually, then retrieves weather data based on location
 */
 
-// Automatically obtains the user's location, if supported
+// Automatically obtain the user's location, if supported
 function getLocation() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(findCoords, locationError);
@@ -11,13 +11,11 @@ function getLocation() {
 	}
 }
 
-/* 
-	Finds the user's coordinates and updates the HTML to reflect the user's location. 
-	Also finds the weather based on found location
-*/
+// Find weather based on user's determined coordinates and update HTML
 function findCoords(position) {
 	let lat = position.coords.latitude;
 	let long = position.coords.longitude;
+	let mapPosition = {lat: position.coords.latitude, lng: position.coords.longitude};
 	const key = 'AIzaSyC2Mcoh2tL1KeJUbmn420w0lPvPclJJvMQ';
 	let url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ lat + ',' + long + '&key=' + key;
 	
@@ -27,26 +25,30 @@ function findCoords(position) {
 
 		document.getElementById('autocomplete').value = city + ', ' + state;
 		getWeather(lat, long);
+		initMap(mapPosition);
 		updateHeader(city);
 	});
 
-	// Removes splashscreen if it is still visible
+	// Remove splashscreen if it is still visible
 	if (document.getElementById("splashscreen")) {
 		removeSplash();
 	}
 }
 
-// Implements Google Autocomplete, and updates HTML and finds weather based on selection
-$(function searchLocation() {
+// Implement Google Autocomplete and find weather based on selection
+function searchLocation() {
 
 	// Autocomplete and listener for main search bar
 	let autocomplete = new google.maps.places.Autocomplete(document.querySelector('#autocomplete'));
 	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+		document.getElementById('autocomplete').blur();
 		let place = this.getPlace();
 		let lat = place.geometry.location.lat();
 		let long = place.geometry.location.lng();
+		let mapPosition = {lat: lat, lng: long};
 		let city = place.address_components[3].long_name;
 		getWeather(lat, long);
+		initMap(mapPosition);
 		updateHeader(city);
 	});
 
@@ -67,7 +69,7 @@ $(function searchLocation() {
 		updateHeader(splahCity);
 		removeSplash();
 	});
-});
+}
 
 // Find the weather, given the user's latitude and longitude
 function getWeather(lat, long) {
@@ -75,11 +77,20 @@ function getWeather(lat, long) {
 
 	// For testing purposes, cors-anywhere has been added to allow access to the Dark Sky API locally
 	$.getJSON('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + weatherKey + '/' + lat + ',' + long).done(function(data) {
-		
+
+		// Change font size of hourly summary if content is too long
+		if (data.hourly.summary.length > 110) {
+			document.getElementById('hourlysummary').style.fontSize = '14pt';
+			getElementById('hourlycontent').style.paddingTop = '13px';
+		} else if (data.hourly.summary.length > 100) {
+			document.getElementById('hourlysummary').style.fontSize = '15pt';
+			getElementById('hourlycontent').style.paddingTop = '12px';
+		}
+
 		// Update HTML to reflect retrieved weather data
 		$('#temp').html(Math.round(data.currently.temperature) + '°F');
 		$('#condition').html(data.minutely.summary);
-		$('#wind').html('<strong>Wind:</strong> ' + Math.round(data.currently.windSpeed) + ' mph');
+		$('#wind').html('<strong>Wind:</strong> ' + Math.round(data.currently.windSpeed) + ' mph ' + getWindDirection(data.currently.windBearing));
 		$('#humidity').html('<strong>Humidity:</strong> ' + Math.round(data.currently.humidity * 100) + '%');
 		$('#dewpoint').html('<strong>Dew Point:</strong> ' + Math.round(data.currently.dewPoint) + '°');
 		$('#pressure').html('<strong>Pressure:</strong> ' + Math.round(data.currently.pressure) + ' mb');
@@ -90,38 +101,32 @@ function getWeather(lat, long) {
 		$('#hourlysummary').html(data.hourly.summary);
 		$('#dailysummary').html(data.daily.summary);
 
-		// Update hourly data
-		for (let j = 1; j < 11; j++) {
-			document.getElementById('hourtime' + j).innerHTML = getTime(data.hourly.data[j].time);
-			document.getElementById('hourtemp' + j).innerHTML = Math.round(data.hourly.data[j].temperature) + '°';
-		}
-
-		// Update daily data
-		for (let j = 1; j < 8; j++) {
-			document.getElementById('dayofweek' + j).innerHTML = getDayOfWeek(data.daily.data[j].time);
-			document.getElementById('high' + j).innerHTML = Math.round(data.daily.data[j].temperatureHigh) + '°';
-			document.getElementById('low' + j).innerHTML = Math.round(data.daily.data[j].temperatureLow) + '°';
-		}
-
-		// Loads Skycons, and adds the icon for current conditions to the page
+		// Load Skycons and add the icon for current condition to the page
 		let icons = new Skycons({'color': '#000000'}),
      	list  = ['clear-day', 'clear-night', 'partly-cloudy-day', 'partly-cloudy-night', 'cloudy', 'rain', 'sleet', 'snow', 'wind', 'fog'], i;
 
-  		for(i = list.length; i--;) {
+  		for (i = list.length; i--;) {
 			let weatherType = list[i], elements = document.getElementsByClassName(weatherType);
 
-			for (e = elements.length; e--;){
+			for (e = elements.length; e--;) {
    				icons.set(elements[e], weatherType);
 			}
 		}
 
-		// Adds icons to page
 		icons.add(document.getElementById('icon'), data.currently.icon);
 		
+		// Update hourly data and icons
 		for (let i = 1; i < 11; i++) {
+			document.getElementById('hourtime' + i).innerHTML = getTime(data.hourly.data[i].time);
+			document.getElementById('hourtemp' + i).innerHTML = Math.round(data.hourly.data[i].temperature) + '°';
 			icons.add(document.getElementById('houricon' + i), data.hourly.data[i].icon);
 		}
+
+		// Update daily data and icons
 		for (let j = 1; j < 8; j++) {
+			document.getElementById('dayofweek' + j).innerHTML = getDayOfWeek(data.daily.data[j].time);
+			document.getElementById('high' + j).innerHTML = Math.round(data.daily.data[j].temperatureHigh) + '°';
+			document.getElementById('low' + j).innerHTML = Math.round(data.daily.data[j].temperatureLow) + '°';
 			icons.add(document.getElementById('dayicon' + j), data.daily.data[j].icon);
 		}
 
@@ -129,7 +134,7 @@ function getWeather(lat, long) {
 	});
 }
 
-// Returns time of day in 00:00 AM/PM format based off time retrieved from JSON data
+// Return time of day in 00:00 AM/PM format based off time retrieved from JSON data
 function getTime(unixTime) {
 
 	// Convert from milliseconds to seconds
@@ -138,18 +143,22 @@ function getTime(unixTime) {
 	let meridiem = '';
 
 	switch (jsTime.getHours()) {
+
 		case 0:
 			hour = 12;
 			meridiem = 'AM';
 			break;
+			
 		case 12:
 			hour = 12;
 			meridiem = 'PM';
 			break;
+
 		default:
 			if (jsTime.getHours() < 12) {
 				hour = jsTime.getHours();
 				meridiem = 'AM';
+				
 			} else {
 				hour = (jsTime.getHours() - 12);
 				meridiem = 'PM';
@@ -162,7 +171,7 @@ function getTime(unixTime) {
 	return time;
 }
 
-// Returns day of the week based off JSON data
+// Return day of the week based off JSON data
 function getDayOfWeek(unixTime) {
 
 	// Convert from milliseconds to seconds
@@ -170,27 +179,35 @@ function getDayOfWeek(unixTime) {
 	let day = '';
 
 	switch (jsTime.getDay()) {
+
 		case 0:
 			day = 'Sun';
 			break;
+
 		case 1:
 			day = 'Mon';
 			break;
+
 		case 2:
 			day = 'Tue';
 			break;
+
 		case 3:
 			day = 'Wed';
 			break;
+
 		case 4:
 			day = 'Thu';
 			break;
+
 		case 5:
 			day = 'Fri';
 			break;
+
 		case 6:
 			day = 'Sat';
 			break;
+			
 		default:
 			console.log('Day of week could not be retrieved.');
 	}
@@ -198,21 +215,50 @@ function getDayOfWeek(unixTime) {
 	return day;
 }
 
-// Alerts user when their location cannot be found
+// Return the wind direction
+function getWindDirection(angle) {
+
+	let directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'];
+	return directions[Math.floor(((angle + (360 / 16) / 2) % 360) / (360 / 16))];
+}
+
+// Alert user when their location cannot be found
 function locationError() {
 	window.alert('Unable to retrieve location.');
 }
 
-// Updates current header to reflect selected location
+// Update current header to reflect selected location
 function updateHeader(city) {
 	document.getElementById('currentheader').innerHTML = 'Currently in ' + city;
 	$('#faveicon').fadeIn();
 }
 
+// Create a Google Map for the baselayer of the radar
 function initMap(position) {
+
+	let imageArray = [];
+
 	let map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 40, lng: -83},
-		zoom: 8,
-		disableDefaultUI: true
+		center: position,
+		zoom: 10,
+		disableDefaultUI: true,
+		mapTypeId: 'terrain'
 	});
+
+	let marker = new google.maps.Marker({
+		position: position,
+		map: map
+	});
+
+	tileNEX = new google.maps.ImageMapType ({
+		getTileUrl: function(tile, zoom) {
+			return 'https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913/' + zoom + '/' + tile.x + '/' + tile.y + '.png?' + (new Date()).getTime();
+		},
+		tileSize: new google.maps.Size(256, 256),
+		opacity: 0.60,
+		name: 'NEXRAD',
+		isPng: true
+	});
+
+	map.overlayMapTypes.setAt('1', tileNEX);
 }
